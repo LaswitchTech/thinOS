88761368abfba8d6aa796e10887441fefed4699d
Louis Ouellet <louis_ouellet@hotmail.com>
2025-11-24 14:20:54 -0500

    General: Starting Dev on thinOS Configuration panel.

--- src/main.py
+++ src/main.py
@@ -11,7 +11,8 @@ def main():
     # Just show the configuration dialog and nothing else
     app.configuration.show()
 
-    sys.exit(app.exec_())
+    # When the dialog is closed (Save or Cancel), just exit
+    sys.exit(0)
 
 if __name__ == "__main__":
     main()
