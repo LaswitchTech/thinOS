e2639537dbba37a9bf23c02bbdaf701dcdf88dc0
Louis Ouellet <louis_ouellet@hotmail.com>
2025-11-24 14:30:08 -0500

    General: Completed Dev on thinOS Configuration panel.

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -5,9 +5,8 @@ from .application import Application
 from .helper import Helper
 from .configuration import Configuration
 from .log import Log
-from .window import Window
 from .ui import MsgBox, StepIndicator, Form
 
 __version__ = "1.0.0"
 
-__all__ = ["Application", "Helper", "Configuration", "Log", "Window", "MsgBox", "StepIndicator", "Form"]
+__all__ = ["Application", "Helper", "Configuration", "Log", "MsgBox", "StepIndicator", "Form"]
