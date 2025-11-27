2d72bb561246469936160b7a1621ab7a383a3beb
Louis Ouellet <louis_ouellet@hotmail.com>
2025-11-27 14:29:42 -0500

    General: Updated dependencies.

--- setup.sh
+++ setup.sh
@@ -59,6 +59,8 @@ if [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "debian" ]; then
     sudo apt-get install -y imagemagick feh || true
     sudo apt-get install -y freerdp2-x11 || true
     sudo apt-get install -y freerdp3-x11 || true
+    sudo apt-get install -y liburiparser1 || true
+    sudo apt-get install -y libcjson1 || true
     sudo apt-get install -y openvpn || true
     sudo apt-get install -y wireguard-tools || true
     sudo apt-get install -y python3 || true
