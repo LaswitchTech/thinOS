b33bf0197349e1889a7cfa532ea92e67b267c7dc
Louis Ouellet <louis_ouellet@hotmail.com>
2025-11-28 08:13:41 -0500

    General: Added the missing library.

--- setup.sh
+++ setup.sh
@@ -63,6 +63,7 @@ if [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "debian" ]; then
     sudo apt-get install -y imagemagick feh || true
     # sudo apt-get install -y freerdp2-x11 || true
     # sudo apt-get install -y freerdp3-x11 || true
+    sudo apt-get install -y libswscale6 || true
     sudo apt-get install -y liburiparser1 || true
     sudo apt-get install -y libcjson1 || true
     sudo apt-get install -y openvpn || true
